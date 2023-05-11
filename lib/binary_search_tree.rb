ed1b859a2204bcabb5a8edf605e640800419c804
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-05-11 09:38:48 +0100

    commit tree 1ad01f4affd04cefbebb463e6124cc02204a589a

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,4 +1,5 @@
 require_relative './node'
+require 'byebug'
 
 class BinarySearchTree
   attr_accessor :root
@@ -8,11 +9,53 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+    node = @root
+  
+    return nil if node == nil
+  
+    while node
+      return node if node.value == value
+  
+      if node.value > value
+        node = node.left
+      else
+        node = node.right
+      end
+    end
+  
+    nil
   end
 
   def insert(value)
-    # your code here
+    new_node = Node.new(value)
+
+    if @root.nil?
+      @root = new_node
+      return new_node
+    else
+      current_node = @root
+
+      while true
+        if value < current_node.value
+          if current_node.left.nil?
+            current_node.left = new_node
+            return new_node
+          else
+            current_node = current_node.left
+          end
+        elsif value > current_node.value
+          if current_node.right.nil?
+            current_node.right = new_node
+            return new_node
+          else
+            current_node = current_node.right
+          end
+        else
+          # The value is equal to the current node's value, so we don't allow duplicates.
+          return nil
+        end
+      end
+    end
   end
 
 end 
